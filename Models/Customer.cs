e9ba8234fd935861711d23ef86ddcd9491a1c6f3
Luc654 <lucas.dieleman@outlook.com>
2026-01-31 12:23:43 +0100

    Feat: Add customer page

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -11,22 +11,27 @@ public partial class Customer
 {
     [Key]
     [Column("customerID")]
+    [Required]
     public int CustomerId { get; set; }
 
     [Column("firstName")]
     [StringLength(50)]
+    [Required]
     public string FirstName { get; set; } = null!;
 
     [Column("lastName")]
     [StringLength(50)]
+    [Required]
     public string LastName { get; set; } = null!;
 
     [Column("email")]
     [StringLength(100)]
+    [Required]
     public string? Email { get; set; }
 
     [Column("phoneNumber")]
     [StringLength(20)]
+    [Required]
     public string? PhoneNumber { get; set; }
 
     [InverseProperty("Customer")]
--- Services/CustomerService.cs
+++ Services/CustomerService.cs
@@ -27,4 +27,5 @@ public class CustomerService
         _db.Customers.Add(customer);
         await _db.SaveChangesAsync();
     }
+    
 }
